9385d9ae3a2d2a54a99a2a2907c1cb451d8f0d83
unknown <Fedulov.Kirill17@gmail.com>
2020-12-08 01:20:49 +0300

    Major rewrite

--- nn.py
+++ nn.py
@@ -1,7 +1,148 @@
+from typing import NamedTuple
 import glob
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Loss functions
+
+class mse:
+	def loss(self, x, y):
+		return np.sum((y-x)**2)/2
+
+	def grad(self, x, y):
+		return x-y
+
+# Layers
+
+class layer:
+	def __init__(self):
+		self.p = {}
+		self.g = {}
+
+class linear(layer):
+	def __init__(self, isize, osize):
+		super().__init__()
+
+		self.p['w'] = np.random.randn(isize, osize)
+		self.p['b'] = np.random.randn(osize)
+
+	def fwd(self, x):
+		self.x = x
+		return x@self.p['w']+self.p['b']
+
+	def bck(self, g):
+		self.g['w'] = self.x.T@g
+		self.g['b'] = np.sum(g, axis=0)
+		return g@self.p['w'].T
+
+class activation(layer):
+	def __init__(self, f, fp):
+		super().__init__()
+
+		self.f  = f
+		self.fp = fp
+
+	def fwd(self, x):
+		self.x = x
+		return self.f(x)
+
+	def bck(self, g):
+		return self.fp(self.x)*g
+
+def fsigmoid(x):
+	return 1/(1+np.exp(-x))
+
+def fsigmoid_prime(x):
+	s = fsigmoid(x)
+	return s*(1-s)
+
+class sigmoid(activation):
+	def __init__(self):
+		super().__init__(fsigmoid, fsigmoid_prime)
+
+def ftanh(x):
+	return np.tanh(x)
+
+def ftanh_prime(x):
+	return 1-ftanh(x)**2
+
+class tanh(activation):
+	def __init__(self):
+		super().__init__(ftanh, ftanh_prime)
+
+def frelu(x):
+	r = np.copy(x)
+	r[r<0] = 0
+	return r
+
+def frelu_prime(x):
+	r = frelu(x)
+	r[x>0] = 1
+	return r
+
+class relu(activation):
+	def __init__(self):
+		super().__init__(frelu, frelu_prime)
+
+# Net
+
+class nn:
+	def __init__(self, L):
+		self.L = L
+
+	def fwd(self, x):
+		for l in self.L:
+			x = l.fwd(x)
+		return x
+
+	def bck(self, g):
+		for l in reversed(self.L):
+			g = l.bck(g)
+		return g
+
+	def png(self):
+		for l in self.L:
+			for n, p in l.p.items():
+				yield p, l.g[n]
+
+# Optimization
+
+class sgd:
+	def __init__(self, lr=0.01):
+		self.lr = lr
+
+	def step(self, N):
+		for p, g in N.png():
+			p -= self.lr*g
+
+# Data
+
+Batch = NamedTuple('Batch', [('x', np.ndarray), ('y', np.ndarray)])
+
+def batches(x, y, n=32, shuffle=True):
+	assert(len(x)==len(y))
+	ss = np.arange(0, len(x), n)
+	if shuffle: np.random.shuffle(ss)
+
+	for s in ss:
+		yield Batch(x[s:s+n], y[s:s+n])
+
+# Training
+
+def train(N, x, y, nepochs=5000, nbatches=32, loss=mse(), optim=sgd()):
+	for e in range(nepochs):
+		l = 0.0
+		for b in batches(x, y, n=nbatches):
+			p  = N.fwd(b.x)
+			l += loss.loss(p, b.y)
+			g  = loss.grad(p, b.y)
+
+			N.bck(g)
+			optim.step(N)
+
+		print('Epoch {} copmleted Loss is {}'.format(e, l/nbatches))
 
 files = glob.glob('data/tests_*.csv')
 d = pd.concat([pd.read_csv(f, sep=';', decimal=',', encoding='cp1251', header=None, skiprows=4, nrows=460) for f in files])
@@ -9,22 +150,10 @@ d = pd.concat([pd.read_csv(f, sep=';', decimal=',', encoding='cp1251', header=No
 dd = d.iloc[:, 2:]
 d_norm = (dd-dd.min())/(dd.max()-dd.min())
 
-X = d_norm.iloc[:, 2:].to_numpy(dtype=np.float64)
+X = d.iloc[:, 2:].to_numpy(dtype=np.float64)
 L = d.iloc[:, 0 ].to_numpy()
 
 L_unique = np.unique(L)
-
-model_shape = [X.shape[1], 16, L_unique.shape[0]]
-n_layers    = len(model_shape)
-
-def init_weights_and_biases(shape):
-	assert(len(shape)>1)
-
-	weights = np.array([np.random.randn(shape[i+1], shape[i]) for i in range(len(shape)-1)], dtype=object)
-	biases  = np.array([np.zeros(shape[i]) for i in range(1, len(shape))], dtype=object)
-	return weights, biases
-
-weights, biases = init_weights_and_biases(model_shape)
 
 def produce_target_values(shape, labels, labels_set):
 	assert(shape[0] == labels.shape[0])
@@ -36,124 +165,18 @@ def produce_target_values(shape, labels, labels_set):
 
 	return target
 
-def mse(target, predicted):
-	assert(target.shape == predicted.shape)
-
-	return np.sum(np.power(target-predicted, 2))/2
-
-def mse_prime(target, predicted):
-	assert(target.shape == predicted.shape)
-
-	return predicted-target
-
-def sigmoid(x):
-	return 1/(1+np.exp(-x))
-
-def sigmoid_prime(x):
-	s = sigmoid(x)
-	return s*(1-s)
-
-def relu(x):
-	result = np.copy(x)
-	result[result<0] = 0
-	return result
-
-def relu_prime(x):
-	result = np.copy(x)
-	result[result<0] = 0
-	result[result>0] = 1
-	return result
-
-n_samples = X.shape[0]
-
-Z = np.empty(n_samples, dtype=object)
-A = np.empty(n_samples, dtype=object)
-
-NB = np.empty(n_samples, dtype=object)
-NW = np.empty(n_samples, dtype=object)
-
-ANB = []
-ANW = []
-
 Y = produce_target_values([L.shape[0], L_unique.shape[0]], L, L_unique)
 
-act       = sigmoid
-act_prime = sigmoid_prime
+N = nn([linear(isize=X.shape[1], osize=64), relu(),
+	    linear(isize=64, osize=L_unique.shape[0]), relu()])
 
-cst       = mse
-cst_prime = mse_prime
+train(N, X, Y, nepochs=5000, nbatches=32, optim=sgd(lr=0.01))
 
-batch_size = 23
-batch_idx  = 0
-batch      = 0
+n_guessed_right = 0
 
-eta = 1
-
-while batch_idx+batch_size<=n_samples:
+for x, y in zip(X, Y):
+	p = N.fwd(x)
 	
-	anb = np.array([np.zeros(b.shape) for b in biases],  dtype=object)
-	anw = np.array([np.zeros(w.shape) for w in weights], dtype=object)
-
-	error = 0.0
-
-	for sample_idx in range(batch_size):
-		idx = batch_idx+sample_idx
-
-		x = X[idx]
-
-		zis = np.empty(n_layers, dtype=object)
-		ais = np.empty(n_layers, dtype=object)
-		
-		zis[0] = np.copy(x)
-		ais[0] = np.copy(x)
-
-		for i, (w, b) in enumerate(zip(weights, biases)):
-			zis[i+1] = np.dot(w, ais[i])+b
-			ais[i+1] = act(zis[i+1])
-
-		Z[idx] = zis
-		A[idx] = ais
-
-		y = Y[idx]
-
-		error += cst(y, ais[-1])
-
-		nb = np.empty(biases.shape[0], dtype=object)
-		nw = np.empty(weights.shape[0], dtype=object)
-
-		d = cst_prime(y, ais[-1])*act_prime(zis[-1])
-		
-		nb[-1] = d
-		nw[-1] = np.outer(d, ais[-2].T)
-
-		for l in range(2, n_layers):
-			z = zis[-l]
-			cp = act_prime(z)
-
-			d = np.dot(weights[-l+1].T, d)*cp
-
-			nb[-l] = d
-			nw[-l] = np.outer(d, ais[-l-1].T)
-
-		NB[idx] = nb
-		NW[idx] = nw
-
-		for accum_b, nabla_b in zip(anb, nb): accum_b += nabla_b
-		for accum_w, nabla_w in zip(anw, nw): accum_w += nabla_w
-
-	ANB.append(anb)
-	ANW.append(anw)
-
-	for b, accum_b in zip(biases, anb):  b -= eta/batch_size*accum_b
-	for w, accum_w in zip(weights, anw): w -= eta/batch_size*accum_w
-
-	print('Batch {} error: {}'.format(batch, error/batch_size))
-
-	batch_idx += batch_size
-	batch     += 1
-
-n_left = n_samples-batch_idx
-
-while batch_idx<n_samples:
-
-	batch_idx += 1
+	if np.argmax(p)==np.argmax(y): n_guessed_right+=1
+	
+print(n_guessed_right, X.shape[0])
