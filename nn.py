a44eca2187cc4a0b828df874bdbc5e8b91536120
unknown <Fedulov.Kirill17@gmail.com>
2020-12-07 04:58:49 +0300

    Backprop done ??

--- nn.py
+++ nn.py
@@ -12,6 +12,7 @@ L = d.iloc[:, 0 ].to_numpy()
 L_unique = np.unique(L)
 
 model_shape = [X.shape[1], 16, L_unique.shape[0]]
+n_layers    = len(model_shape)
 
 def init_weights_and_biases(shape):
 	assert(len(shape)>1)
@@ -21,36 +22,6 @@ def init_weights_and_biases(shape):
 	return weights, biases
 
 weights, biases = init_weights_and_biases(model_shape)
-
-def relu(x):
-	x[x<0] = 0
-	return x
-
-def fwd_once(inputs, weights, biases, act=relu):
-	result = np.dot(weights, inputs)+biases
-	result = act(result)
-	return result
-
-def fwd(inputs, weights, biases, act=relu):
-	neurons = np.empty(inputs.shape[0], dtype=object)
-
-	for i, x in enumerate(inputs):
-		layers = np.empty(weights.shape[0]+1, dtype=object)
-
-		layers[0] = np.copy(x)
-		for j, (w, b) in enumerate(zip(weights, biases)):
-			layers[j+1] = fwd_once(layers[j], w, b, act)
-
-		neurons[i] = layers
-
-	return neurons
-
-neurons = fwd(X, weights, biases)
-
-input_neurons  = neurons[0][0]
-hidden_neurons = neurons[0][1]
-output_neurons = neurons[0][2]
-# print(input_neurons, hidden_neurons, output_neurons)
 
 def produce_target_values(shape, labels, labels_set):
 	assert(shape[0] == labels.shape[0])
@@ -62,11 +33,105 @@ def produce_target_values(shape, labels, labels_set):
 
 	return target
 
-def sel(target, predicted):
+def mse(target, predicted):
 	assert(target.shape == predicted.shape)
 
-	return np.sum(np.power(target-predicted, 2))/target.shape[0]
+	return np.sum(np.power(target-predicted, 2))/2
+
+def mse_prime(target, predicted):
+	assert(target.shape == predicted.shape)
+
+	return predicted-target
+
+def sigmoid(x):
+	return 1/(1+np.exp(-x))
+
+def sigmoid_prime(x):
+	s = sigmoid(x)
+	return s*(1-s)
+
+n_samples = X.shape[0]
+
+Z = np.empty(n_samples, dtype=object)
+A = np.empty(n_samples, dtype=object)
+
+NB = np.empty(n_samples, dtype=object)
+NW = np.empty(n_samples, dtype=object)
+
+ANB = []
+ANW = []
 
 Y = produce_target_values([L.shape[0], L_unique.shape[0]], L, L_unique)
 
-print(output_neurons, Y[0], sel(Y[0], output_neurons))
+act       = sigmoid
+act_prime = sigmoid_prime
+
+cst       = mse
+cst_prime = mse_prime
+
+batch_size = 32
+batch_idx = 0
+
+eta = 1
+
+while batch_idx+batch_size<n_samples:
+
+	anb = np.array([np.zeros(b.shape) for b in biases],  dtype=object)
+	anw = np.array([np.zeros(w.shape) for w in weights], dtype=object)
+
+	for sample_idx in range(batch_size):
+		idx = batch_idx+sample_idx
+
+		x = X[idx]
+
+		zis = np.empty(n_layers, dtype=object)
+		ais = np.empty(n_layers, dtype=object)
+		
+		zis[0] = np.copy(x)
+		ais[0] = np.copy(x)
+
+		for i, (w, b) in enumerate(zip(weights, biases)):
+			zis[i+1] = np.dot(w, ais[i])+b
+			ais[i+1] = act(zis[i+1])
+
+		Z[idx] = zis
+		A[idx] = ais
+
+		y = Y[idx]
+
+		nb = np.empty(biases.shape[0], dtype=object)
+		nw = np.empty(weights.shape[0], dtype=object)
+
+		d = cst_prime(y, ais[-1])*act_prime(zis[-1])
+		
+		nb[-1] = d
+		nw[-1] = np.outer(d, ais[-2].T)
+
+		for l in range(2, n_layers):
+			z = zis[-l]
+			cp = act_prime(z)
+
+			d = np.dot(weights[-l+1].T, d)*cp
+
+			nb[-l] = d
+			nw[-l] = np.outer(d, ais[-l-1].T)
+
+		NB[idx] = nb
+		NW[idx] = nw
+
+		for accum_b, nabla_b in zip(anb, nb): accum_b += nabla_b
+		for accum_w, nabla_w in zip(anw, nw): accum_w += nabla_w
+
+	ANB.append(anb)
+	ANW.append(anw)
+
+	for b, accum_b in zip(biases, anb):  b -= eta/batch_size*accum_b
+	for w, accum_w in zip(weights, anw): w -= eta/batch_size*accum_w
+
+	batch_idx += batch_size
+
+n_left = n_samples-batch_idx
+
+while batch_idx<n_samples:
+
+	batch_idx += 1
